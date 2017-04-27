f496faf13c33b7c6e748b8dbe26d50a4d0415693
Leo Tulipan <Leo@LeoTulipan.at>
2017-04-27 07:56:26 +0200

    Refactor code to class. Still alpha (only shows cat&tag of first item)

--- README.md
+++ README.md
@@ -2,17 +2,34 @@
 
 Based on idea of https://github.com/dreikanter/wp2md
 
+# Current state: early ALPHA
+
+I am still playing around with the code and copying the working features from wp2md.py
+
+# Next Steps
+
+- Get Channel data ("def dump_channel" in the python module)
+- Get item data ("dump_item")
+- [Command Line Arguments and Interface](https://github.com/vilic/clime)
+- Take Filename, output & date arguments
+
+# Setup 
+
 ## Configure git ssh on Windows
 
 1. Add openssh Key to git https://github.com/settings/keys
 2. [SSH to Server via GUI to get around "Store key in cache?" error](https://stackoverflow.com/questions/33240137/git-clone-pull-continually-freezing-at-store-key-in-cache)
 3. Set the env Var GIT_SSH:
 
-	`export GIT_SSH="C:\Program Files (x86)\PuTTY\plink.exe`
+    export GIT_SSH="C:\Program Files (x86)\PuTTY\plink.exe"
 
 or in Powershell:
 
     [Environment]::SetEnvironmentVariable("GIT_SSH", "C:\Program Files (x86)\PuTTY\plink.exe", "User")
+
+test with: 
+
+    ssh -T git@github.com
 
 4. Set upstream: 
 
@@ -30,9 +47,14 @@ Prerequisites:
 
     npm install
 
-Compile Typescript:
+# Running the code
 
-    tsc wp2md.ts
-Run:
+See also https://stackoverflow.com/questions/33535879/how-to-run-typescript-files-from-command-line for ts-node option
 
-    node wp2md.js
+## Compile (and watch) Typescript:
+
+    tsc -w wp2md.ts
+
+## Run:
+
+    node wp2md    
--- wp2md.ts
+++ wp2md.ts
@@ -2,14 +2,97 @@
 // npm install "@types/node" --save-dev
 import * as fs from "fs"
 
-let xml = fs.readFileSync('test.xml', 'utf8')
 
-let parseString = require('xml2js').parseString;
+class wpParser {
 
+    // # XML elements to save (starred ones are additional fields
+    // # generated during export data processing)
+    WHAT2SAVE = {
+        'channel': [
+            'title',
+            'description',
+            'author_display_name',
+            'author_login',
+            'author_email',
+            'base_site_url',
+            'base_blog_url',
+            'export_date', //# Generated: data export timestamp
+            'content', //# Generated: items list
+            // # 'link',
+            // # 'language',
+        ],
+        'item': [
+            'title',
+            'link',
+            'creator',
+            'description',
+            'post_id',
+            'post_date',
+            'post_date_gmt',
+            'comment_status',
+            'post_name',
+            'status',
+            'post_type',
+            'excerpt',
+            'content', //# Generated: item content
+            'comments', //# Generated: comments list
+            'category', //# we want multiple <category domain="post_tag"
+            // # 'guid',
+            // # 'is_sticky',
+            // # 'menu_order',
+            // # 'ping_status',
+            // # 'post_parent',
+            // # 'post_password',
+        ],
+        'comment': [
+            'comment_id',
+            'comment_author',
+            'comment_author_email',
+            'comment_author_url',
+            'comment_author_IP',
+            'comment_date',
+            'comment_date_gmt',
+            'comment_content',
+            'comment_approved',
+            'comment_type',
+            // # 'comment_parent',
+            // # 'comment_user_id',
+        ]
+    }
 
-// https://github.com/Leonidas-from-XIV/node-xml2js
-parseString(xml, function (err, result) {
-    console.dir(
-        (result["rss"]["channel"][0]["item"][0]["category"])
-    )
-});
+    // # Wordpress RSS items to public - static page header fields mapping#(undefined names will remain unchanged)
+    FIELD_MAP = {
+        'creator': 'author',
+        'post_date': 'created',
+        'post_date_gmt': 'created_gmt',
+    }
+
+    constructor(private xmlFile: string) {
+
+    }
+
+    // Categories and Tags look like this:
+    //
+    // result["rss"]["channel"][0]["item"][0]["category"]
+    //
+    // [ { _: 'Blog', '$': { domain: 'category', nicename: 'blog' } },
+    //   { _: 'Fett', '$': { domain: 'post_tag', nicename: 'fett' } },
+    //   { _: 'Omega-6 Fett',
+    //     '$': { domain: 'post_tag', nicename: 'omega-6' } } ]
+    parse2js() {
+
+        let parseString = require('xml2js').parseString;
+        // https://github.com/Leonidas-from-XIV/node-xml2js
+        parseString(this.xmlFile, function (err, result) {
+            console.dir(
+                (result["rss"]["channel"][0]["item"][0]["category"])
+            )
+        });
+    }
+
+}
+
+let xmlFile = fs.readFileSync('test.xml', 'utf8')
+let parser = new wpParser(xmlFile)
+
+parser.parse2js()
